c58c148c48d6ace24671bcc8edd7947d5d46166d
RobertoCeccon <cecconroberto7@gmail.com>
2025-07-04 21:18:22 -0300

    Update forca.py

--- FORCA/forca.py
+++ FORCA/forca.py
@@ -2,7 +2,7 @@ import random
 
 nome = input('Digite o seu nome!')
 
-print(f'Bem-vindo, {nome}. Vamos jogar um jogo?\nEu escolho uma palavra e você tenta adivinhar.')
+print(f'Bem-vindo, {nome}. Vamos jogar um jogo?\nEu escolho uma palavra e você tenta adivinhar.\nEscolha um tema:')
 lista_de_temas = [['Girafa', 'Tartaruga', 'Elefante', 'Borboleta'], ['Abacaxi', 'Espaguete', 'Panqueca', 'Chocolate'], ['Noruega', 'Egito', 'Argentina', 'Madagascar']]
 escolha_de_tema = (int(input('1. Animais\n2. Comidas\n3. Países')) - 1)
 
@@ -16,3 +16,4 @@ for i in forca:
     tracinho = tracinho + '-'
 
 print(tracinho)
+
